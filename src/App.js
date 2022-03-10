9ba27ffdd52c907f20554da69132e4c531b8a74e
wisnuciwun <adiwardhanawisnu@gmail.com>
2022-03-11 06:47:09 +0700

    learn part 3 - useMemo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 function App() {
+
+  //-------useState-------//
 
   // if (condition) {
   //   const [input, setInput] = useState(0) --->  musn't put useState on this, because it is run for same render order
   // }
-
-  function initial() {
-    console.log("masuk");
-    return 2
-  }
-
-  const [inputObj, setInputObj] = useState({ count: 1, color: 'red' })
-
-  const count = inputObj.count
-  const color = inputObj.color
-
-  const [input, setInput] = useState(0)
-  const [inputLazy, setInputFunctionInit] = useState(() => {
-    console.log("masuk");
-    return 4
-  }) // --> setState with lazy initial (not always render)
 
   function decrementDouble() {
     // setInput(input - 1)
@@ -49,6 +35,26 @@ function App() {
     })
   }
 
+  //-------useState-------//
+
+  //-------useEffect-------//
+
+  function initial() {
+    console.log("masuk");
+    return 2
+  }
+
+  const [inputObj, setInputObj] = useState({ count: 1, color: 'red' })
+
+  const count = inputObj.count
+  const color = inputObj.color
+
+  const [input, setInput] = useState(0)
+  const [inputLazy, setInputFunctionInit] = useState(() => {
+    console.log("masuk");
+    return 4
+  }) // --> setState with lazy initial (not always render)
+
   const [result, setResult] = useState('Users')
   const [item, setItem] = useState([])
   const [windowWidth, setWindow] = useState(window.innerWidth)
@@ -73,6 +79,22 @@ function App() {
     } // --> unmount method
   }, [])
 
+  //-------useEffect-------//
+
+  //-------useMemo-------//
+
+  const [number, setnumber] = useState(0)
+  const [dark, setdark] = useState(false)
+  const doubleNum = useMemo(() => {return slowFunction(number)}, [number])  
+  const themeChoosen = useMemo(() => {return {backgroundColor: dark ? 'black': 'white', color: dark ? 'black': 'white'}}, [dark])
+
+  useEffect(() => {
+    console.log("masuqq")
+  }, [themeChoosen])
+  
+
+  console.log("faewfa", doubleNum);
+
   return (
     <div className="App">
       USE STATE TESTING
@@ -88,17 +110,30 @@ function App() {
       </header>
       USE EFFECT TESTING
       <header className="App-header">
-        {/* <button onClick={() => setResult('Users')} >Users</button>
+        <button onClick={() => setResult('Users')} >Users</button>
         <button onClick={() => setResult('Comments')} >Comments</button>
         <button onClick={() => setResult('Posts')} >Posts</button>
         <p>{result}</p>
         {item.map(val => {
           return <pre>{JSON.stringify(val)}</pre>
-        })} */}
+        })}
         {windowWidth}
+      </header>
+      USE MEMO TESTING
+      <header className="App-header">
+        <input type="number" value={number} onChange={(e) => setnumber(parseInt(e.target.value))} />
+        <button onClick={() => setdark(prevDark => !prevDark)} >Change Theme</button>
+        <div style={themeChoosen} >{doubleNum}<input type="number" disabled value={doubleNum} /></div>
       </header>
     </div>
   );
+}
+
+function slowFunction(num) {
+  console.log("masuk");
+  for (let i = 0; i <= 100000000000000; i++) {
+    return num * 2
+  }
 }
 
 export default App;
